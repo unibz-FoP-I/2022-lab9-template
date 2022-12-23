acc4e4fac5a1059d124f858b01356cbfa0176d3c
sarah <sarwinkler@unibz.it>
2022-12-23 21:50:19 +0100

    add solution

--- src/main.c
+++ src/main.c
@@ -4,12 +4,32 @@
 
 #include "street.h"
 
+// size of line buffer when reading file
 #define LINE_BUF_SIZE 2048
 
-int read_streets(FILE *fp);
+// linked list data structure to store street list
+struct street_list_struct {
+  StreetData data;
+  struct street_list_struct *next;
+};
+typedef struct street_list_struct StreetList;
 
+
+int read_streets(FILE *fp, StreetList **list);
+void print_streets(StreetList *list);
+int translate(StreetList *list, char *town, char *street);
+void free_list(StreetList *list);
+
+
+/**
+ * @brief Main function, takes file as argument or reads from stdin
+ * @param argc number of command line arguments
+ * @param argv command line arguments, 1st argument can be csv input file
+ * @return 0 if searched address was found, 1 otherwise
+ **/
 int main(int argc, char const *argv[])
 {
+    // set file pointer to input file
     FILE *fp = stdin;
     // if an argument is given, use it as input file
     if (argc > 1) {
@@ -21,16 +41,54 @@ int main(int argc, char const *argv[])
         }
     }
 
-    // read data from file
-    int result = read_streets(fp);
+    // read csv file and store entries in linked list
+    StreetList *list;
+    int result = read_streets(fp, &list);
 
+    // close file after reading
     if (argc > 1)
-        fclose(fp);
+      fclose(fp);
 
-    exit(result);
+    // if file has not been read correctly, exit
+    if (result)
+      exit(1);
+
+    // print all streets in list
+    //print_streets(list);
+
+    // ask the user for the town to search
+    printf("Enter the Italian town name:\n");
+    char searched_town[50];
+    scanf("%49[^\n]", searched_town);
+    
+    // consume newline character before getting next input
+    char temp;
+    scanf("%c", &temp);
+    
+    // ask the user for the street name to search for
+    printf("Enter the Italian street name:\n");
+    char searched_street[50];
+    scanf("%49[^\n]", searched_street);
+
+    // search in list for translation
+    int not_found = translate(list, searched_town, searched_street);
+
+    // free StreetList
+    free_list(list);
+
+    // return 0 if searched street was found
+    exit(not_found);
 }
 
-int read_streets(FILE *fp) {
+
+/**
+ * @brief This function takes a file pointer to a csv file of streets and sets
+ *        *list to the head of a linked list containing all streets
+ * @param fp file pointer
+ * @param list that is filled
+ * @return 0 if reading succeeded and 1 otherwise
+ **/
+int read_streets(FILE *fp, StreetList **list) {
     // buffer to hold line
     static char line_buf[LINE_BUF_SIZE];
     char *line = NULL;
@@ -43,24 +101,96 @@ int read_streets(FILE *fp) {
     }
 
     StreetData street;
+    // head will point to the first element of the list
+    StreetList *head = NULL;
+    // prev is used to remember the last element inserted into the list
+    StreetList *prev = NULL;
     while (getline(&line, &len, fp) != -1) {
         // copy line into line buffer
         strncpy(line_buf, line, sizeof(line_buf));
         // remove trailing newline
         line_buf[sizeof(line_buf) - 1] = 0;
         // parse line as csv
+        // note that the function was modified to trim all entries of the csv,
+        // i.e., to cutoff leading and trailing spaces
         if (csv_row_to_street(line_buf, &street)) {
-            puts("------------------------------");
-            // print parsed street data record
-            show_street(&street);
+            // could print parsed street data record
+            //show_street(&street);
 
-            // TODO: insert the data in street into a list
-        
+            // allocate memory for next list element
+            StreetList *current = malloc(sizeof(StreetList));
+            //copy data into next list element 
+            current->data = street;
+            // if head is NULL, the current element is the first, so set head.
+            // otherwise, set current element as next of previous one 
+            if (head == NULL) {
+                head = current;
+            } else {
+                prev->next = current;
+            }
+            prev = current;
         } else {
             fprintf(stderr, "Cannot parse: %s\n", line);
         }
     }
 
     free(line);
+    // set head of list to the dedicated parameter to "return" it
+    *list = head;
+
     return EXIT_SUCCESS;
+}
+
+/**
+ * @brief This function takes a linked list of streets, and prints it
+ * @param list StreetList to print
+ **/
+void print_streets(StreetList *list){
+    StreetList *current = list;
+    // walk the list and print all elements
+    while(current){
+        show_street(&(current->data));
+        current = current->next;
+    }
+}
+
+/**
+ * @brief This function takes a linked list of streets, and the Italian name of
+ *        a town and street that the user wants to translate
+ * @param list the head of the StreetList
+ * @param town Italian name of town that user wants to find 
+ * @param street Italian name of street that user wants to find
+ * @return 0 if names are found, 1 otherwise
+ **/
+int translate(StreetList *list, char *town, char *street) {
+    StreetList *current = list;
+    // walk through the list until last element reached; if the element was 
+    // found, the loop will be exited directly
+    while(current){
+        StreetData data = current->data;
+        // check whether both twon and street match
+        if (strcmp(data.municipality_it, town) == 0 && 
+          strcmp(data.name_it, street) == 0) {
+          printf("The German name is %s, %s\n", data.municipality_de, data.name_de);
+          return 0;
+        }
+        current = current->next;
+    }
+    // if this point is reached, the sought element was not found
+    printf("No town and street with these names (%s, %s) were found.\n",
+      town, street);
+    return 1;
+}
+
+/**
+ * @brief This function frees the memory of a StreetList.
+ * @param list the head of the StreetList
+ **/
+void free_list(StreetList *list){
+    StreetList *current = list;
+    while(current){
+        StreetList *next = current->next;
+        free(current);
+        current = next;
+    }
 }
--- src/street.c
+++ src/street.c
@@ -5,6 +5,8 @@
 
 #include "street.h"
 
+#define MIN(x,y) (x < y ? x : y)
+
 static char *get_next_token(char* current, char sep) {
     // find position of next separator in string
     char *next = strchr(current, sep);
@@ -17,25 +19,51 @@ static char *get_next_token(char* current, char sep) {
     return 0;
 }
 
+/**
+ * @brief This function takes a pointer to a string and modifies it to remove
+ *        spaces in the front and in the back.
+ * @param str pointer to string, modified by this function
+ **/
+void trim(char **str){
+    // trim left: while space found, advance pointer
+    while (str != NULL && *str != NULL && (*str)[0] == ' ') {
+      *str = (*str)+1;
+    }
+    // trim right: replace spaces by terminating 0 character
+    size_t i = strlen(*str) - 1;
+    while (i>0 && ((*str)[i] == ' ')){
+      (*str)[i] = 0;
+      i--;
+    }
+}
+
+/**
+ * @brief This function parses a csv line into a StreetData record.
+ * @param line to be parsed
+ * @param street StreetDataRecord to be filled
+ **/
 int csv_row_to_street(char *line, StreetData *street) {
     if (street) {
-        // clear StreetData record
+        // clear memory of StreetData record
         memset(street, 0, sizeof(StreetData));
         char *token = line;
-        // find next separator, replace it by 0 and returns its position
+        // find next separator, replace it by 0 and return its position
         char *next_token = get_next_token(line, CSV_SEP);
 
         // set German municipality name
         if (!token) return 0;
-        // copy (at most STR_SIZE-1) characters from token to street data record
-        // (-1 to leave trailing 0)
-        strncpy(street->municipality_de, token, STR_SIZE - 1);
+        // remove spaces from token and copy it to street data record.
+        // copy at most STR_SIZE-1 characters to keep terminating 0
+        // (the 0 is then there because of the memset above)
+        trim(&token);
+        strncpy(street->municipality_de, token, MIN(strlen(token), STR_SIZE-1));
         token = next_token;
         next_token = get_next_token(token, CSV_SEP);
 
         // set Italian municipality name
         if (!token) return 0;
-        strncpy(street->municipality_it, token, STR_SIZE - 1);
+        trim(&token);
+        strncpy(street->municipality_it, token, MIN(strlen(token), STR_SIZE-1));
         token = next_token;
         next_token = get_next_token(token, CSV_SEP);
 
@@ -51,13 +79,15 @@ int csv_row_to_street(char *line, StreetData *street) {
 
         // set German street name
         if (!token) return 0;
-        strncpy(street->name_de, token, STR_SIZE - 1);
+        trim(&token);
+        strncpy(street->name_de, token, MIN(strlen(token), STR_SIZE-1));
         token = next_token;
         next_token = get_next_token(token, CSV_SEP);
 
         // set Italian street name
         if (!token) return 0;
-        strncpy(street->name_it, token, STR_SIZE - 1);
+        trim(&token);
+        strncpy(street->name_it, token, MIN(strlen(token), STR_SIZE-1));
         token = next_token;
         next_token = get_next_token(token, CSV_SEP);
     }
@@ -69,3 +99,4 @@ void show_street(StreetData *street) {
     printf("DE: %20s: %s\n", street->municipality_de, street->name_de);
     printf("code: %d\n", street->code);
 }
+
